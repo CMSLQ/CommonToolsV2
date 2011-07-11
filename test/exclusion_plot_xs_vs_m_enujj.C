6d7682bf493098713ba8fa8ce6447208e06c2188
ferencek <>
2011-07-11 14:37:29 +0000

    eqnuq in the legend changed to enujj

--- test/exclusion_plot_xs_vs_m_enujj.C
+++ test/exclusion_plot_xs_vs_m_enujj.C
@@ -367,7 +367,7 @@ void makePlots()
  //legend->SetFillStyle(0);
  legend->SetTextFont(42);
  legend->SetMargin(0.15);
- legend->SetHeader("LQ#bar{LQ} #rightarrow eq#nuq");
+ legend->SetHeader("LQ#bar{LQ} #rightarrow e#nujj");
  legend->AddEntry(pl,"D#oslash exclusion (1 fb^{-1}), #beta = 0.5","f");
  legend->AddEntry(xsTh_vs_m,"2#beta(1-#beta)#times#sigma_{NLO} with theory uncert., #beta = 0.5","lf");
  legend->AddEntry(xsData_vs_m_expected, "Expected 95% CL upper limit (36 pb^{-1})","lp");
